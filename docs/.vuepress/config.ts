03cbefb3a677cabb7ae251c175c74dd02c8ed005
Jiaolong <45839824+Wang-Jiaolong@users.noreply.github.com>
2022-08-17 13:31:48 +0800

    a

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -33,7 +33,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       // 因为本主题的默认背景色偏向白色，如果 navColor 是 2，建议需要开启(true)，否则白背景 + 白字体 = 看不见
       bgTimeColor: true,     // 是否开启图片的背景色随一天的不同时间而变化，并且开启时间窗口提示，默认是 false。时间分为四种：白天（原图）、黄昏（偏黄）、晚上（偏黑）、深夜（偏深黑）
       bgTimeColorArray: ['transparent', 'rgba(255, 148, 48, .2)', 'rgba(0, 0, 0, .3)', 'rgba(0, 0, 0, .5)'],   // 第一个是白天的颜色（默认原图），第二个是黄昏的颜色，第三个是晚上的颜色，第四个是深夜的颜色。bgTimeColor 为 true 生效。提示：如果不想要这个效果，但是又想要时间窗口提示效果，则改为 ['transparent', 'transparent', 'transparent', 'transparent']
-      descFade: true,   // 是否开启图片中间描述的淡入效果，默认为 false
+      descFade: false,   // 是否开启图片中间描述的淡入效果，默认为 false
       desc: ["@Jiaolong 的个人博客，与你一起发现更大的世界。"],  // 多个描述，如果填写则覆盖 config.js 的 description，不填写默认读取 config.js 的 description，descFadeIn 为 true 生效
       descFontSize: '1.5rem',   // desc 的字体大小，默认 1.4rem。提示：原主题是 1.1rem
       descFadeInTime: 150,  // 描述的淡入效果持续时间，descFade 为 true 生效，默认 200 毫秒
@@ -43,7 +43,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
       bubblePosition: 0,  // 气泡效果的位置，范围：0-100，不同数值代表不同的起始位置，0是整个图片，50是半张图（一半的下方）。bubble 为 true 生效。默认是 0
       bubbleNum: 200,   // 气泡的个数，bubble 为 true 生效，默认 200 个
     },
-    bodyBgImgOpacity: 0.3, // body 背景图透明度，选值 0 ~ 1.0, 默认0.5
+    bodyBgImgOpacity: 0.4, // body 背景图透明度，选值 0 ~ 1.0, 默认0.5
     nav: [
       { text: '首页', link: '/' },
       // {
